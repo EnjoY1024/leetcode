b53c0d44862676cfbdc96035d1fe3befa6c12db6
EnjoY1024 <hrb097@163.com>
2021-02-02 22:58:07 +0800

    add

--- strStr.py
+++ strStr.py
@@ -2,3 +2,16 @@
 # -*- coding: utf-8 -*-
 # @Time : 2021/2/2 22:53
 __author__ = "hanruobing"
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        flag = False
+
+        
