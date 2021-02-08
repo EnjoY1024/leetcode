ad2375404a85641ffc7f1293555b956dbad6e8f2
EnjoY1024 <hrb097@163.com>
2021-02-08 22:06:47 +0800

    add

---
+++ countAndSay.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/8 22:06
+__author__ = "hanruobing"
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
