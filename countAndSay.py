edc5a2cdb05bfa0cdd979dfdb8d8103cbf1d8c63
EnjoY1024 <hrb097@163.com>
2021-02-19 16:52:17 +0800

    add

--- countAndSay.py
+++ countAndSay.py
@@ -9,10 +9,29 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        if n<0 or n>30:
-            return
-        if n = 1:
-            return 11
+        if n == 1:
+            return '1'
         else:
-            x = countAndSay(n-1)
-        
+            key = self.countAndSay(n-1)+'@'
+            #print(key,type(key),len(key),type(len(key)))
+            r = ''
+            j = 0
+            num = 0
+            for i in range(len(key)):
+                if key[j] == key[i]:
+                    num +=1
+                elif key[i] == '@':#如果访问到最后一个可能和前面的数字相同，为了终止，但是好像没用
+                    r += str(num)
+                    r += str(key[i-1])
+                    break
+                else:
+                    r += str(num)
+                    #print(key[i-1])
+                    r += str(key[i-1])
+                    j = i
+                    num = 1
+            return str(r)
+
+s = Solution()
+#s.countAndSay(2)
+print(s.countAndSay(5))
