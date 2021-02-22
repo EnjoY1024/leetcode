e025d92d6336e9dd17b7b9b3cc581afc1fee387d
EnjoY1024 <hrb097@163.com>
2021-02-22 17:36:43 +0800

    add需调

--- addBinary.py
+++ addBinary.py
@@ -12,47 +12,63 @@ class Solution(object):
         """
         len_a = len(a)
         len_b = len(b)
-
+        long = []
+        short = []
         if len_a>len_b:
-            long = a
-            short = b
+            for i in range(len(a)):
+                long.append(str(i))
+    
+            for i in range(len(b)):
+                short.append(str(i))
         else:
-            long = b
-            short = a
+            for i in range(len(b)):
+                long.append(str(i))
+            
+            for i in range(len(a)):
+                short.append(str(i))
+        
+        # print(long,type(long),type(long[0]))
+        # print(short,type(short))
 
+        
         len_short = len(short)
         len_long = len(long)
         jinwei = 0
-        r1 = '0'*len_short
+        r1 = []
+        for i in range(len_short):
+            r1.append('0')
         for i,j in zip(range(len_short-1,-1,-1), range(len_long-1,-1,len_long-len_short+1)):
-            if carry+int(short[i])+int(long[j]) == 0:
+            print(short[i],long[j])
+            if jinwei+int(short[i])+int(long[j]) == 0:
                 r1[i] = '0'
                 jinwei=0
-            elif carry+int(short[i])+int(long[j]) == 1:
+            elif jinwei+int(short[i])+int(long[j]) == 1:
                 r1[i] = '1'
                 jinwei=0
             else:
                 r1[i]= '0'
                 jinwei = 1
-        print(r1)
-        r2='0'*(len_long-len_short)
+        print(r1,type(r1))
+        r2 = []
+        for i in range(len_long-len_short):
+            r2.append('0')
         if jinwei == 0:
-            rr = long[0:len_long-len_short-1] + r1
-            return rr
+            rr = long[0:len_long-len_short-1].append(r1)
+            print(rr,type(rr))
+            return ''.join(rr)
         else:
             for i in range(len_long-len_short-1,-1,-1):
                 if jinwei+int(long[i])==2:
                     jinwei = 1
                     r2[i] = '0'
                 else:
-                    return long[0:len_long-len_short-1] + r1
-            print(r2)
-            return r2+r1
+                    rr = long[0:len_long - len_short - 1].append(r1)
+                    return ''.join(rr)
+            return ''.join(r2).join(r1)
 
 
 s = Solution()
 print(s.addBinary('110','1'))
-
 
 
 
