49188151c65fb300fa805f4973eaa87671efcbb9
EnjoY1024 <hrb097@163.com>
2021-03-04 22:51:13 +0800

    add

--- maxProfit.py
+++ maxProfit.py
@@ -10,3 +10,20 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        minValue = float("inf")
+        res = 0
+        minValue = float("inf")
+        for i in range(len(prices)):
+            if prices[i] < minValue:  # 更新最小值
+                minValue = prices[i]
+
+            if prices[i] - minValue > res:  # 更新最大收益
+                res = prices[i] - minValue
+        print(minValue)
+        return res
+
+
+#两个for遍历相减取最大超时
+
+s = Solution()
+print(s.maxProfit([2,6,1,3]))
